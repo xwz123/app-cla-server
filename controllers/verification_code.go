d3d7c1e2d8f28ed2aea5bc52bd653c0c4a60dba3
xwzQmxx <1499273991@qq.com>
2021-06-10 10:19:14 +0800

    limit email suffix

--- controllers/verification_code.go
+++ controllers/verification_code.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/opensourceways/app-cla-server/config"
+	"github.com/opensourceways/app-cla-server/dbmodels"
 	"github.com/opensourceways/app-cla-server/email"
 	"github.com/opensourceways/app-cla-server/models"
 )
@@ -12,25 +13,34 @@ type VerificationCodeController struct {
 	baseController
 }
 
-func (this *VerificationCodeController) Prepare() {
-	this.apiPrepare("")
+func (vc *VerificationCodeController) Prepare() {
+	vc.apiPrepare("")
 }
 
 // @Title Post
 // @Description send verification code when signing
 // @Param	:org_cla_id	path 	string					true		"org cla id"
 // @Param	:email		path 	string					true		"email of corp"
+// @Param   :applyTo    path    string                  true        "corporation  employee or individual"
 // @Success 201 {int} map
 // @Failure util.ErrSendingEmail
-// @router /:link_id/:email [post]
-func (this *VerificationCodeController) Post() {
+// @router /:link_id/:email/:applyTo [post]
+func (vc *VerificationCodeController) Post() {
 	action := "create verification code"
-	linkID := this.GetString(":link_id")
-	emailOfSigner := this.GetString(":email")
+	linkID := vc.GetString(":link_id")
+	emailOfSigner := vc.GetString(":email")
+	applyTo := vc.GetString(":applyTo")
+
+	if applyTo == dbmodels.ApplyToCorporation {
+		if err := models.CheckRestrictEmailSuffix(emailOfSigner); err != nil {
+			vc.sendModelErrorAsResp(err, action)
+			return
+		}
+	}
 
 	orgInfo, merr := models.GetOrgOfLink(linkID)
 	if merr != nil {
-		this.sendFailedResponse(0, "", merr, action)
+		vc.sendFailedResponse(0, "", merr, action)
 		return
 	}
 
@@ -38,11 +48,11 @@ func (this *VerificationCodeController) Post() {
 		emailOfSigner, linkID, config.AppConfig.VerificationCodeExpiry,
 	)
 	if err != nil {
-		this.sendModelErrorAsResp(err, action)
+		vc.sendModelErrorAsResp(err, action)
 		return
 	}
 
-	this.sendSuccessResp("create verification code successfully")
+	vc.sendSuccessResp("create verification code successfully")
 
 	sendEmailToIndividual(
 		emailOfSigner, orgInfo.OrgEmail,
@@ -52,6 +62,59 @@ func (this *VerificationCodeController) Post() {
 		),
 		email.VerificationCode{
 			Email:      emailOfSigner,
+			Org:        orgInfo.OrgAlias,
+			Code:       code,
+			ProjectURL: orgInfo.ProjectURL(),
+		},
+	)
+}
+
+//@Title CodeWithFindPwd
+//@Description send verification code when find password
+//@Param platform path  string true "code platform"
+//@Param org_repo path  string true "org:repo"
+//@Param email    query string true "email of contributor"
+//@Success 201 {int} map
+//@Failure 400 util.ErrSendingEmail
+//@router /:platform/:org_repo [get]
+func (vc *VerificationCodeController) CodeWithFindPwd() {
+	action := "create verification code when find password"
+	cmEmail := vc.GetString("email")
+	org, repo := parseOrgAndRepo(vc.GetString(":org_repo"))
+	linkID, err := models.GetLinkID(buildOrgRepo(vc.GetString(":platform"), org, repo))
+	if err != nil {
+		vc.sendFailedResponse(400, string(models.ErrNoLinkOrNoManager), err, action)
+		return
+	}
+
+	orgInfo, err := models.GetOrgOfLink(linkID)
+	if err != nil {
+		vc.sendFailedResponse(400, string(models.ErrNoLinkOrNoManager), err, action)
+		return
+	}
+
+	manager, dbErr := dbmodels.GetDB().ListCorporationManager(linkID, cmEmail, "")
+	if dbErr != nil || len(manager) == 0 {
+		reason := fmt.Errorf("can't find the corporation manager by email")
+		vc.sendFailedResponse(400, errUnknownEmailPlatform, reason, action)
+		return
+	}
+
+	code, err := models.CreateVerificationCode(
+		cmEmail, linkID, config.AppConfig.VerificationCodeExpiry,
+	)
+	if err != nil {
+		vc.sendModelErrorAsResp(err, action)
+		return
+	}
+
+	vc.sendSuccessResp("create verification code successfully")
+
+	sendEmailToIndividual(
+		cmEmail, orgInfo.OrgEmail,
+		"Verification code for retrieve password ",
+		email.FindPasswordVerifyCode{
+			Email:      cmEmail,
 			Org:        orgInfo.OrgAlias,
 			Code:       code,
 			ProjectURL: orgInfo.ProjectURL(),
